30a1f6645423e148445575506f5bfaa9541045b5
Mikhail Chertkov <mishaschertkov@gmail.com>
2026-03-19 03:32:16 +0400

    add deleteButton listener

--- index.js
+++ index.js
@@ -20,13 +20,24 @@ function loadTasks() {
 }
 
 function createItem(item) {
-	const template = document.getElementById("to-do__item-template");
-	const clone = template.content.querySelector(".to-do__item").cloneNode(true);
+  const template = document.getElementById("to-do__item-template");
+  const clone = template.content.querySelector(".to-do__item").cloneNode(true);
   const textElement = clone.querySelector(".to-do__item-text");
   const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
   const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
   const editButton = clone.querySelector(".to-do__item-button_type_edit");
+  
+  textElement.textContent = item;
 
+  deleteButton.addEventListener('click', () => {
+    clone.remove();
+    const curTasks = getTasksFromDOM();
+    saveTasks(curTasks);
+  });
+
+
+
+  return clone;
 }
 
 function getTasksFromDOM() {
